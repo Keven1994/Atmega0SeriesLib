97327d3c9102e9feb1f4b827aa771c012e1df4dd
keven <keven.kloeckner@gmail.com>
2019-03-22 19:57:15 +0100

    twi file added

---
+++ GccApplication1/hw_abstractions/TWI.hpp
@@ -0,0 +1,25 @@
+
+/*
+ * TWI.hpp
+ *
+ * Created: 22.03.2019 19:50:16
+ *  Author: keven
+ */
+
+namespace AVR {
+	
+	namespace twi {
+		
+		namespace details {
+			template<typename component, typename instance>
+			struct TWI {
+				
+			};	
+		}
+		
+		
+		
+		
+	}
+	
+}
